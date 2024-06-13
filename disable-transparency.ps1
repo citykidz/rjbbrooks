635d6ae36cb2904d9f4d051c6334b9904462e5ea
citykidz <33699543+citykidz@users.noreply.github.com>
2024-06-13 15:51:42 -0600

    Powershell to turn off transparency in Windows 10

---
+++ disable-transparency.ps1
@@ -0,0 +1,3 @@
+﻿# Disable Transparent Colors in Settings > Personalization
+$registryPath = "HKCU:\\SOFTWARE\\MICROSOFT\\WINDOWS\\CURRENTVERSION\\THEMES\\PERSONALIZE"
+Set-ItemProperty -Path $registryPath -Name "EnableTransparency" -Value 0
